dd4b44528bf004cf0ff652bd36068dddb245b9fb
João Vitor Rosera <jvrosera01@gmail.com>
2025-01-28 17:43:04 -0300

    Add files via upload

---
+++ 31_objetos_com_metodos/js/scripts.js
@@ -0,0 +1,18 @@
+let pessoa = {
+    nome: "João",
+    idade: 19,
+    falar: function() {
+        console.log("Olá, tudo bem?");
+    },
+    soma: function(a,b) {
+        return a + b;
+    }
+};
+
+console.log(pessoa.nome);
+
+pessoa.falar();
+
+var soma = pessoa.soma(2,2);
+
+console.log(soma);
---
+++ 33_acessando_o_dom/js/scripts.js
@@ -0,0 +1,18 @@
+// Tag
+var titulo = document.getElementsByTagName('h1')[[0]];
+
+console.log(titulo);
+
+var lis = document.getElementsByTagName('li');
+
+console.log(lis[3]);
+
+// Id
+var paragrafo = document.getElementById('paragrafo');
+
+console.log(paragrafo);
+
+// Class
+var itensDaLista = document.getElementsByClassName('item');
+
+console.log(itensDaLista);
